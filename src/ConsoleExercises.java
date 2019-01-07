586e3a08f3e1859e1bfb67b46aa212c381f03aa4
Lexi Morgan <leximorgan816@gmail.com>
2019-01-07 17:28:17 -0600

    everything is finished except the sentence is being skipped

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -16,7 +16,25 @@ public class ConsoleExercises {
         String userInputFirst = scanner.next();
         String userInputSecond = scanner.next();
         String userInputThird = scanner.next();
-        System.out.format("You entered the number: %d. You entered the words: %s, %s, %s", userInteger, userInputFirst, userInputSecond, userInputThird);
+
+        System.out.format("%nEnter a Sentence%n");
+        String userSentence = scanner.nextLine();
+        scanner.nextLine();
+
+//this prompt gets executed but it doesn't wait for the input to move on.
+
+        System.out.format("%nWe are going to calculate the perimeter of the classroom.%n");
+        System.out.format("%nEnter the length in feet%n");
+        int length = Integer.parseInt(scanner.nextLine());
+
+        System.out.format("%nEnter The width in feet%n");
+        int width = Integer.parseInt(scanner.nextLine());
+
+        int perimeter = (length*2) + (width*2);
+        int area = length * width;
+        System.out.format("You entered the number: %d. You entered the words: %s, %s, %s. Your sentence was: %s", userInteger, userInputFirst, userInputSecond, userInputThird, userSentence);
+
+        System.out.format("%n The perimeter of the room is %d feet. The area of the room is %d square feet.", perimeter, area);
 
 
     }
