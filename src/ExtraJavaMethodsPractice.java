b8c9d983408384db1f9d7522ad796909c90abf70
Lexi Morgan <leximorgan816@gmail.com>
2019-01-10 17:01:00 -0600

    finished isVowel and hasVowel

--- src/ExtraJavaMethodsPractice.java
+++ src/ExtraJavaMethodsPractice.java
@@ -4,6 +4,10 @@ public class ExtraJavaMethodsPractice {
         System.out.println(secondChar("New string here!"));
         System.out.println(lastChar("New string here!"));
         System.out.println(secondToLastChar("New string here!"));
+        System.out.println(isVowel("A"));
+        System.out.println(isVowel("B"));
+        System.out.println(hasVowel("Dog"));
+        System.out.println(hasVowel("www"));
 
 
 //  ***** This closes the Main Method *****
@@ -57,15 +61,40 @@ public class ExtraJavaMethodsPractice {
 //  9. Write a method named countEvens(start, end) that returns an integer containing the integer that is the count of all even numbers between the start and the end input integers.
 //
 //  10. Write a method named isVowel() that accepts a String input of length 1 and returns a boolean if that string is a vowel other than "y".
-//
+
+    public static boolean isVowel(String input) {
+        if (input.length() > 1){
+            return false;
+        }
+         return input.equalsIgnoreCase("a") ||
+                input.equalsIgnoreCase("e") ||
+                input.equalsIgnoreCase("i") ||
+                input.equalsIgnoreCase("o") ||
+                input.equalsIgnoreCase("y");
+        }
+
+
 //  11. Write a method named hasVowels() that accepts a string of any length and returns a boolean if there are any vowels in that string.
 //
+     public static boolean hasVowel(String input){
+                String letter = "";
+
+                for (int i=0; i< input.length(); i++){
+                    letter = input.charAt(i) + "";
+                    if(isVowel(letter)) {
+                        return true;
+                    }
+                }
+               return false;
+     }
+
 //  12. Write a method named countVowels() that accepts a string of any length and returns an integer count of the number of vowels in the provided input String.
-//
+
+
 //  13. Write a solution to FizzBuzz using recursion instead of a loop.
-//
+
 //  14. Write a method named isPrime() that that accepts in an integer number and returns a boolean if the number is evenly divisible only by either 1 or the number itself.
-//
+
 //  15. Write a method named getTwentyPrimes() that returns a string containing the first 20 prime numbers, each separated by a comma. Output: "1, 2, 3, 5, 7, 11, 13, 17, 19"... until we have a total count of 20 primes in the string.
 
 
