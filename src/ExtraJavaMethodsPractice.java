f7b8c4f3fc1b5d54ee34c8199198910c0912842a
Lexi Morgan <leximorgan816@gmail.com>
2019-01-10 20:49:13 -0600

    finished up to fizzBuzz

--- src/ExtraJavaMethodsPractice.java
+++ src/ExtraJavaMethodsPractice.java
@@ -1,19 +1,31 @@
+import java.util.Scanner;
+
 public class ExtraJavaMethodsPractice {
     public static void main(String[] args) {
-        System.out.println(firstChar("New string here!"));
-        System.out.println(secondChar("New string here!"));
-        System.out.println(lastChar("New string here!"));
-        System.out.println(secondToLastChar("New string here!"));
-        System.out.println(isVowel("A"));
-        System.out.println(isVowel("B"));
-        System.out.println(hasVowel("Dog"));
-        System.out.println(hasVowel("www"));
-
+//        System.out.println(firstChar("New string here!"));
+//        System.out.println(secondChar("New string here!"));
+//        System.out.println(lastChar("New string here!"));
+//        System.out.println(secondToLastChar("New string here!"));
+//        System.out.println(userWantsToContinue());
+//        System.out.println(userWantsToContinue());
+//        System.out.println(isEven(6));
+//        System.out.println(isEven(13));
+//        System.out.println(isOdd(45));
+//        System.out.println(isOdd(22));
+//        System.out.println(countsOdds(1, 5));
+//        System.out.println(countsEvens(3, 10));
+//        System.out.println(isVowel("A"));
+//        System.out.println(isVowel("B"));
+//        System.out.println(hasVowel("Dog"));
+//        System.out.println(hasVowel("www"));
+//        System.out.println(countVowels("aaabbbcccdddeee"));
+        System.out.println(fizzBuzz(30));
+//        System.out.println(forLoopFizzBuzz(30));
 
 //  ***** This closes the Main Method *****
     }
 
-//  TODO: //////////////////////////////////////////////////////////////////////////
+//  TODO: //////////////////////////////////////////////////////////////////////////////////////////
 //  Java Methods Practice:
 
 //  1. Write a method named firstChar() that takes a string as an input and returns the first letter as a character data type.
@@ -41,25 +53,62 @@ public class ExtraJavaMethodsPractice {
         return userInput.charAt(length-2);
     }
 
-
 //  5. Write a method named userWantsToContinue(). This method should prompt the user if they want to continue and then return a boolean value if the user inputs "y" or "yes".
-
-//    public static boolean userWantsToContinue(String userInput){
-//        if (userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y")) {
-//            boolean continueYes = true;
-//        } else {
-//            System.out.println("You chose not to continue.");
-//        } return continueYes;
-//    }
+    public static boolean userWantsToContinue(){
+        Scanner scan = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("Do you want to continue? [y/n]");
+        String userInput = scan.next();
+        if (userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 //  6. Write a method named isEven() that takes in an integer and returns a boolean if the input is even or not.
-//
+
+    public static boolean isEven(int input) {
+        if (input % 2 == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 //  7.Write a method named isOdd() that takes in an integer and returns a boolean if the input is odd or not.
-//
+
+    public static boolean isOdd(int input) {
+        if (input % 2 != 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 //  8. Write a method named countOdds(start, end) that returns an integer containing the integer that is the count of all odd numbers between the start and the end input integers.
-//
+
+    public static int countsOdds(int start, int end){
+            int oddCounter = 0;
+        for (int i = start; i<=end; i++){
+            if (isOdd(i)){
+                oddCounter++;
+            }
+        } return oddCounter;
+    }
+
+
 //  9. Write a method named countEvens(start, end) that returns an integer containing the integer that is the count of all even numbers between the start and the end input integers.
-//
+
+    public static int countsEvens(int start, int end){
+            int evenCounter = 0;
+         for(int i = start; i<= end; i++) {
+             if (isEven(i)){
+                 evenCounter++;
+             }
+         } return evenCounter;
+    }
+
+
 //  10. Write a method named isVowel() that accepts a String input of length 1 and returns a boolean if that string is a vowel other than "y".
 
     public static boolean isVowel(String input) {
@@ -70,7 +119,7 @@ public class ExtraJavaMethodsPractice {
                 input.equalsIgnoreCase("e") ||
                 input.equalsIgnoreCase("i") ||
                 input.equalsIgnoreCase("o") ||
-                input.equalsIgnoreCase("y");
+                input.equalsIgnoreCase("u");
         }
 
 
@@ -90,8 +139,62 @@ public class ExtraJavaMethodsPractice {
 
 //  12. Write a method named countVowels() that accepts a string of any length and returns an integer count of the number of vowels in the provided input String.
 
+     public static int countVowels(String input){
+        int vowelCounter = 0;
+        String letter = "";
+        for (int i = 0; i<= input.length()-1; i++){
+            letter = input.charAt(i) + "";
+            if (isVowel(letter)) {
+                vowelCounter++;
+            }
+        } return vowelCounter;
+
+     }
 
 //  13. Write a solution to FizzBuzz using recursion instead of a loop.
+
+    public static String fizzBuzz(int number) {
+        String emptyString = "All done!";
+        if (number >= 0) {
+            if (number % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (number % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (number % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (number % 3 != 0 && number % 5 != 0) {
+                System.out.println(number);
+            }
+            number -= 1;
+
+            return fizzBuzz(number);
+
+        } return emptyString;
+    }
+
+
+
+//    public static String forLoopFizzBuzz(int number) {
+//        String output = "";
+//        for(int i = 1; i<=number; i++){
+//            if (number % 15 == 0) {
+//                output += "FizzBuzz\n";
+////                System.out.println("FizzBuzz");
+//            } else if (number % 3 == 0) {
+////                System.out.println("Fizz");
+//                output += "Fizz\n";
+//            } else if (number % 5 == 0) {
+////                System.out.println("Buzz");
+//                output += "Buzz\n";
+//            } else {
+//                output += (i + "");
+//            }
+//        }
+//            return output;
+//    }
+
+
+
 
 //  14. Write a method named isPrime() that that accepts in an integer number and returns a boolean if the number is evenly divisible only by either 1 or the number itself.
 
