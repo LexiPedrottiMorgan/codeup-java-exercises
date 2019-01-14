13f1e80972cbd643cc67420576372618e85914d6
Lexi Morgan <leximorgan816@gmail.com>
2019-01-14 16:08:25 -0600

    modified

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -7,15 +7,16 @@ public class CircleApp {
 
 
             Input input = new Input();
+            System.out.println(input.yesNo());
             double radius = input.getDouble();
             System.out.println("The double entered:" + radius);
             Circle circle = new Circle(radius);
             System.out.format("The area of a circle with a radius of %.2f is %.2f. %n", radius, circle.getArea());
             System.out.format("The circumference of a circle with a radius of %.2f is %.2f. %n", radius, circle.getCircumference());
             System.out.println("Do you want to continue making circles?");
-            System.out.println(input.yesNo());
 
         }
+
 
 //closes the main class:
     }
