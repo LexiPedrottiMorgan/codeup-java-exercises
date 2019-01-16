e0e61a891a9b4acfa44cb2ced1f0b18f749d697f
Lexi Morgan <leximorgan816@gmail.com>
2019-01-16 16:07:51 -0600

    rectangle class first commit

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,32 @@
+package shapes;
+
+public class Rectangle {
+
+    protected int length;
+    protected int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getArea(){
+        int area = length*width;
+        return area;
+    }
+
+
+    public int getPerimeter(){
+        int perimeter = (2*length) + (2*width);
+        return perimeter;
+    }
+
+}
